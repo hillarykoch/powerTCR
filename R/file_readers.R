eab20acb2d3c69a58707ddb971071e0cf2aaed57
hillarykoch <hillary.koch01@gmail.com>
2021-03-15 19:54:15 -0400

    handle tcR again

--- R/file_readers.R
+++ R/file_readers.R
@@ -1,23 +1,23 @@
 ##--------------------------------------------------------------------
 ## Borrow functionality from the immunarch package to read in files
 ##--------------------------------------------------------------------
-
-parseFile <- function(path, inframe = TRUE){
-    if(!(is.character(path))){
-        stop("file must be a character string.")
-    }
-    
-    if(!(is.logical(inframe))){
-        stop("inframe must be TRUE or FALSE.")
-    }
-    
-    data <- immunarch::repLoad(.path = path)$data[[1]]
-    counts <- data$Clones
-
-    if(inframe){
-        data <- immunarch::coding(data)
-    }
-
-    counts <- data$Clones
-    counts
-}
+#
+#parseFile <- function(path, inframe = TRUE){
+#    if(!(is.character(path))){
+#        stop("file must be a character string.")
+#    }
+#    
+#    if(!(is.logical(inframe))){
+#        stop("inframe must be TRUE or FALSE.")
+#    }
+#    
+#    data <- immunarch::repLoad(.path = path)$data[[1]]
+#    counts <- data$Clones
+#
+#    if(inframe){
+#        data <- immunarch::coding(data)
+#    }
+#
+#    counts <- data$Clones
+#    counts
+#}
